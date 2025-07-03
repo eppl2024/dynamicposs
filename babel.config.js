c01d165ffd4137b9db407966faf84d5d2b4210f1
eppl2024 <eyppl2024@gmail.com>
2025-07-03 13:54:21 +0545

    Fix Babel configuration to resolve TypeScript transpilation error

--- babel.config.js
+++ babel.config.js
@@ -2,10 +2,7 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: [
-      ['babel-preset-expo', {
-        // Include expo-modules-core in untranspiledModules to ensure proper TypeScript transpilation
-        untranspiledModules: ['expo-modules-core']
-      }],
+      ['babel-preset-expo'],
     ],
   };
 };
